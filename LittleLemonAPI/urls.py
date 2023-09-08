3a7cbabbaf94e53d44d8140c4eef3155a688af70
Sheerwill <willymuange6119@gmail.com>
2023-09-08 17:48:35 +0300

    enabling url settings for api for getting tokens

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu-items/', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('message/', views.msg),
+    path('api-token-auth/', obtain_auth_token)
 ]
