199f09e72e75af47a27588f21429e37b95adefd0
Sheerwill <willymuange6119@gmail.com>
2023-09-08 15:27:13 +0300

    decorators

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('menu-items/', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('message/', views.msg),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from . models import MenuItem
 from . serializers import MenuItemSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 
 # Create your views here.
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
 
 
 
