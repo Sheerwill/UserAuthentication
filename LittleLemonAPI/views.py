a36fe51c0bdf788255020f2d0e4eb34dbe5f9302
Sheerwill <willymuange6119@gmail.com>
2023-09-08 15:10:38 +0300

    ..

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from . models import MenuItem
+from . serializers import MenuItemSerializer
+
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
